75c4d151ef3f8ba5ae0b84f88e0cae1dbcb71441
Tianwei Liu <tianwei.liu@outlook.com>
2024-01-11 22:34:55 -0500

    main: some rust hack

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
 //! A simple benchmark program that computes 
 //! 10_000_000th prime number
-fn is_prime(x: u32, prime_vec: &Vec<u32>) -> bool {
-    let mut pos = 0;
-    loop {
-        if prime_vec[pos] * prime_vec[pos] > x {
-            break;
-        } else if x % prime_vec[pos] == 0 {
-            return false;
-        }
-        pos += 1;
-    }
-    true
+
+macro_rules! PRIME_COUNT {
+    () => {
+        10_000_000
+    };
+}
+
+fn is_prime(x: u32, prime_vec: &[u32]) -> bool {
+    prime_vec.iter().take_while(|&&p| p * p <= x).all(|&p| x % p != 0)
 }
 
 fn main() {
-    let mut prime_vec = Vec::new();
+    let mut prime_vec = Vec::with_capacity(PRIME_COUNT!());
     prime_vec.push(2);
     let mut num_to_check = 3;
-    loop {
+    while prime_vec.len() < PRIME_COUNT!() {
         if is_prime(num_to_check, &prime_vec) {
-            prime_vec.push(num_to_check.clone());
+            prime_vec.push(num_to_check);
         }
-        num_to_check += 1;
-        if prime_vec.len() == 10_000_000 {
-            break;
-        }
+        num_to_check += 2; // Increment by 2 to skip even numbers
     }
-    println!("{}", prime_vec[prime_vec.len() - 1]);
+
+    println!("{}", prime_vec.last().unwrap());
 }
