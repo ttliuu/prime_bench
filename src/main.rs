49e1673b7c28da3dbff383e38cc2bff55f8098c4
Tianwei Liu <tianwei.liu@outlook.com>
2024-01-11 21:52:13 -0500

    main: format

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-
+//! A simple benchmark program that computes 
+//! 10_000_000th prime number
 fn is_prime(x: u32, prime_vec: &Vec<u32>) -> bool {
     let mut pos = 0;
     loop {
@@ -12,27 +13,18 @@ fn is_prime(x: u32, prime_vec: &Vec<u32>) -> bool {
     true
 }
 
-
 fn main() {
     let mut prime_vec = Vec::new();
     prime_vec.push(2);
-
     let mut num_to_check = 3;
     loop {
-
         if is_prime(num_to_check, &prime_vec) {
             prime_vec.push(num_to_check.clone());
         }
-
         num_to_check += 1;
-
-
-
         if prime_vec.len() == 10_000_000 {
             break;
         }
     }
     println!("{}", prime_vec[prime_vec.len() - 1]);
-
-
 }
