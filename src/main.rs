15795e9f6c0b64ee28413eddc8a7846ab821e67b
Tianwei Liu <tianwei.liu@outlook.com>
2024-01-13 20:04:21 -0500

    main: adds built-in time display

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 //! A simple benchmark program that computes 
 //! 10_000_000th prime number
 
+use std::time::SystemTime;
+
 macro_rules! PRIME_COUNT {
     () => {
         10_000_000
@@ -15,12 +17,16 @@ fn main() {
     let mut prime_vec = Vec::with_capacity(PRIME_COUNT!());
     prime_vec.push(2);
     let mut num_to_check = 3;
+    let start = SystemTime::now();
     while prime_vec.len() < PRIME_COUNT!() {
         if is_prime(num_to_check, &prime_vec) {
             prime_vec.push(num_to_check);
         }
         num_to_check += 2; // Increment by 2 to skip even numbers
     }
+    let stop = SystemTime::now();
+    let time = stop.duration_since(start).unwrap();
 
     println!("{}", prime_vec.last().unwrap());
+    println!("time: {}s", time.as_secs_f32());
 }
